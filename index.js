2ba69ad07828e8f54c39a56d459e4f090212e1ff
kkznch <kkznch710md@gmail.com>
2023-02-02 20:34:32 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const clickHereButton = document.getElementById('click-here');
+  clickHereButton.addEventListener('click', function () {
+    alert('ハロー！');
+  });
+});
